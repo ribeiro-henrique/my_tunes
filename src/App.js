83d96fca90f7f918cbdd61a50102324dfafdc3af
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-04-13 17:59:39 -0300

    requisito 1 izi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>Trybe Tunes</p>);
+    return (
+      <div>
+        <h1>Minha Homepage</h1>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route path="" component={ NotFound } />
+      </div>
+    );
   }
 }
 
