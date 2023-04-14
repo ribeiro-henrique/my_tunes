98aff593cb7a6768d1c4bdd6eec3c4bbe3694dc0
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-04-14 18:33:20 -0300

    requisito 5 em tempo record

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,41 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    name: '',
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      name: value,
+    });
+  };
+
   render() {
+    const { name } = this.state;
+
+    const DOIS = 2;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-inp">
+            Busque pelo artista:
+            <input
+              value={ name }
+              onChange={ this.handleChange }
+              id="search-inp"
+              data-testid="search-artist-input"
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            disabled={ name.length < DOIS }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
